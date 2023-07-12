3dd60d5929a6101249771efd0f76506566a419c3
Daniel Zoppi <danzoppi@hotmail.com>
2023-07-12 14:56:26 -0300

    atualização

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.urls import path
-from blog.views import index
+from blog.views import index, post, page, created_by, category
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('post/<slug:slug>/', post, name='post'),
+    path('page/<slug:slug>/', page, name='page'),
+    path('created_by/<int:author_pk>/', created_by, name='created_by'),
+    path('category/<slug:slug>/', category, name='category'),
 ]
 
 
